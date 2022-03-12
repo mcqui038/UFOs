a5da19333830805ead6cbb059119bc69991ce160
Kelly McQuillan <kellymcquillan@Kellys-MacBook-Air.local>
2022-03-11 21:00:13 -0600

    Finished through 11.5.4

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tableData = data;
 // regerence the HTML table using d3
 var tbody = d3.select("tbody");
 
+// build table from data.js
 function buildTable(data) {
     tbody.html("");
     data.forEach((dataRow) => {
@@ -15,3 +16,17 @@ function buildTable(data) {
         );
       });
 }
+
+// build filter function
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    buildTable(filteredData);
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
